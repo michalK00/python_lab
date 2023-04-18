6c0b7308bda1d7dcbaed65bcd0c54ea12688609e
Pawel Dudek <pawel200216@interia.eu>
2023-04-18 08:36:44 +0200

    added a necessary regex explanation comment

--- L6/SSHUser.py
+++ L6/SSHUser.py
@@ -7,5 +7,9 @@ class SSHUser:
         self.last_login_date = last_login_date
 
     def validate(self):
+        # ^ - starts at the beginning, makes sure that there aren't incorrect chars there
+        # [a-z_] - first char is a lower case letter or a _
+        # [a-z0-9_-]{0,31} - up to 31 chars that are letters, numbers or _
+        # $ - asserts position at the end. makes sure that there aren't any incorrect chars after the correct ones
         user_name_regex = r"^[a-z_][a-z0-9_-]{0,31}$"
         return re.search(user_name_regex, self.user_name)
