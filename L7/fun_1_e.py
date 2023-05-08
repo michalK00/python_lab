49744ede6d6cff03d1338a962b447205eade359f
DudasOn <pawel200216@interia.eu>
2023-05-08 13:34:18 +0200

    Minor fix, couldn't amend change

--- L7/fun_1_e.py
+++ L7/fun_1_e.py
@@ -4,8 +4,8 @@ from typing import List, Any
 def flatten(to_flatten: List[Any]) -> List[Any]:
     def inner_flattener(to_flatten_inner: List[Any], acc: List[Any]) -> List[Any]:
         match to_flatten_inner:
-            case [list(), *tail]:
-                return inner_flattener(tail, acc + inner_flattener(to_flatten_inner[0], []))
+            case [[*nested], *tail]:
+                return inner_flattener(tail, acc + inner_flattener(nested, []))
             case [head, *tail]:
                 return inner_flattener(tail, acc + [head])
             case _:
