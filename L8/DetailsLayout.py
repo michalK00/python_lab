50ac0ade410407a66b25f744d0b50705055dd4cd
MichalK <95657053+michalK00@users.noreply.github.com>
2023-05-15 09:14:20 +0200

    repaired sound and images

--- L8/DetailsLayout.py
+++ L8/DetailsLayout.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QStackedWidget, QPushButton, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout
+from PySide6.QtWidgets import QStackedWidget, QPushButton, QLineEdit, QLabel, QVBoxLayout, QHBoxLayout, QSizePolicy
 from PySide6.QtGui import Qt, QPixmap
 from typing import Callable
 from os import path
@@ -10,12 +10,20 @@ ERROR_IMG_FILEPATH: str = "./img/error.png"
 OTHER_IMG_FILEPATH: str = "./img/other.jpg"
 PASSWORD_DENIED_FILEPATH: str = "./img/password_denied.png"
 ERROR_MUSIC_FILEPATH: str = "./sounds/boom.mp3"
-ERROR_MUSIC_VOLUME: float = 0.3
+ERROR_MUSIC_VOLUME: float = 1
 
 
 class DetailsLayout(QVBoxLayout):
-    def __init__(self):
+    def __init__(self, app):
         super().__init__()
+
+        self.audio: QAudioOutput = QAudioOutput()
+        self.audio.setVolume(ERROR_MUSIC_VOLUME)
+
+        self.media_player: QMediaPlayer = QMediaPlayer(parent=app)
+        self.media_player.setAudioOutput(self.audio)
+        self.media_player.setSource(
+            QUrl.fromLocalFile(ERROR_MUSIC_FILEPATH))
 
         self.ip = self._create_labeled_input("IP Address:")
         self.user = self._create_labeled_input("User:")
@@ -41,6 +49,7 @@ class DetailsLayout(QVBoxLayout):
         col2_layout.addWidget(self.pid.input)
 
         main_layout = QVBoxLayout()
+
         details_layout = QHBoxLayout()
         details_layout.addLayout(col1_layout)
         details_layout.addLayout(col2_layout)
@@ -56,6 +65,10 @@ class DetailsLayout(QVBoxLayout):
         error_img.setPixmap(QPixmap(ERROR_IMG_FILEPATH))
         pass_den_img = QLabel()
         pass_den_img.setPixmap(QPixmap(PASSWORD_DENIED_FILEPATH))
+
+        other_img.setScaledContents(True)
+        error_img.setScaledContents(True)
+        pass_den_img.setScaledContents(True)
 
         # QPixmap.scaled(other_img.pixmap(), 150, 300)
         # QPixmap.scaled(error_img.pixmap(), 150, 300)
@@ -82,16 +95,7 @@ class DetailsLayout(QVBoxLayout):
     def _handle_msg_type(self, type: TypeOfMessage):
         match type:
             case type.ERROR:
-                # nie działa :(
-                audio: QAudioOutput = QAudioOutput()
-                audio.setVolume(ERROR_MUSIC_VOLUME)
-
-                media_player: QMediaPlayer = QMediaPlayer()
-                media_player.setAudioOutput(audio)
-                media_player.setSource(
-                    QUrl.fromLocalFile(ERROR_MUSIC_FILEPATH))
-                media_player.setLoops(media_player.Loops.Infinite)
-                media_player.play()
+                self.media_player.play()
                 img = QLabel()
                 img.setPixmap(QPixmap(ERROR_IMG_FILEPATH))
                 self.imgs.setCurrentIndex(1)
@@ -101,15 +105,19 @@ class DetailsLayout(QVBoxLayout):
                 self.imgs.setCurrentIndex(2)
 
         self.imgs.setVisible(True)
+
         self.msg_type.input.setText(str(type.name))
 
     def _create_labeled_input(self, label_text):
         label = QLabel(label_text)
         input = QLineEdit()
         input.setReadOnly(True)
+
+        label.setAlignment(Qt.AlignmentFlag.AlignRight)
         input.setAlignment(Qt.AlignmentFlag.AlignRight)
         input.setMaximumWidth(200)
         input.setContentsMargins(0, 20, 0, 0)
+        label.setContentsMargins(0, 20, 0, 0)
         return LabeledInput(label, input)
 
 
