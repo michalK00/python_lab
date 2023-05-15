741f34c654a3566f2b500d1c237b0711f47710fa
DudasOn <pawel200216@interia.eu>
2023-05-15 14:20:07 +0200

    Started L9

--- L9/SSHLogEntry.py
+++ L9/SSHLogEntry.py
@@ -1,14 +1,15 @@
 import re
 from datetime import datetime
 from type_enum import TypeOfMessage as Msg
-import ipaddress
+from ipaddress import IPv4Address
 from abc import ABCMeta, abstractmethod
+from typing import Match, List, Tuple
 
 
-def get_message_type(row):
-    pass_acc = "Accepted password"
-    pass_den = "Failed password"
-    error = "error:"
+def get_message_type(row: str) -> Msg:
+    pass_acc: str = "Accepted password"
+    pass_den: str = "Failed password"
+    error: str = "error:"
 
     if re.search(pass_acc, row):
         return Msg.PASSWORD_ACCEPTED
@@ -20,80 +21,80 @@ def get_message_type(row):
         return Msg.OTHER
 
 
-def get_date(row):
-    date_regex = r'([A-z]{3})\s+(0?[1-9]|[12][0-9]|3[01]) (?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)'
-    match = re.search(date_regex, row)
+def get_date(row: str) -> datetime:
+    date_regex: str = r'([A-z]{3})\s+(0?[1-9]|[12][0-9]|3[01]) (?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)'
+    match: Match[str] = re.search(date_regex, row)
     if match:
-        month = match.group(1)
-        day = match.group(2)
-        hour = match.group(3)
-        minute = match.group(4)
-        second = match.group(5)
+        month: str = match.group(1)
+        day: str = match.group(2)
+        hour: str = match.group(3)
+        minute: str = match.group(4)
+        second: str = match.group(5)
 
-        date_string = f"{day} {month} {datetime.now().year} {hour}:{minute}:{second}"
-        date_obj = datetime.strptime(date_string, "%d %b %Y %H:%M:%S")
+        date_string: str = f"{day} {month} {datetime.now().year} {hour}:{minute}:{second}"
+        date_obj: datetime = datetime.strptime(date_string, "%d %b %Y %H:%M:%S")
         return date_obj
 
 
-def get_pid(row):
-    pid_regex = r'sshd\[(\d+)\]'
-    match = re.search(pid_regex, row)
+def get_pid(row: str) -> int:
+    pid_regex: str = r'sshd\[(\d+)\]'
+    match: Match[str] = re.search(pid_regex, row)
     if match:
         return int(match.group(1))
 
 
-def get_user_from_log(row):
-    user_regex = r'user (\w+)'
-    match = re.search(user_regex, row)
+def get_user_from_log(row: str) -> str:
+    user_regex: str = r'user (\w+)'
+    match: Match[str] = re.search(user_regex, row)
     if match:
         return match.group(1)
 
 
-def get_ipv4s_from_log(row):
-    ip_regex = r'(?:\d{1,3}\.){3}\d{1,3}'
-    match = re.findall(ip_regex, row)
+def get_ipv4s_from_log(row: str) -> IPv4Address | None:
+    ip_regex: str = r'(?:\d{1,3}\.){3}\d{1,3}'
+    match: List[str] = re.findall(ip_regex, row)
     if match:
         try:
-            return ipaddress.IPv4Address(match[0])
-        except:
+            return IPv4Address(match[0])
+        except ...:
             return None
     return None
 
 
 class SSHLogEntry(metaclass=ABCMeta):
-    def __init__(self, log, message_type):
-        self.date = get_date(log)
-        self.user = get_user_from_log(log)
-        self.ip_v4 = get_ipv4s_from_log(log)
-        self.message_type = message_type
-        self.pid = get_pid(log)
-        self._raw_log = log
+    def __init__(self, log: str, message_type: Msg):
+        self.date: datetime = get_date(log)
+        self.user: str = get_user_from_log(log)
+        self.ip_v4: IPv4Address = get_ipv4s_from_log(log)
+        self.message_type: Msg = message_type
+        self.pid: int = get_pid(log)
+        self._raw_log: str = log
 
-    def __str__(self):
+    def __str__(self) -> str:
         return ("PID: " + str(self.pid) + " date: " + str(self.date) +
                 (f" user: {str(self.user)}" if self.user is not None else "") +
                 " ip_v4: " + str(self.ip_v4) + " message_type: " + str(self.message_type))
 
     @abstractmethod
-    def validate(self):
+    def validate(self) -> bool:
         return (self.date == get_date(self._raw_log)
                 and self.user == get_user_from_log(self._raw_log)
                 and self.ip_v4 == get_ipv4s_from_log(self._raw_log)
                 and self.message_type == get_message_type(self._raw_log)
                 and self.pid == get_pid(self._raw_log))
 
-    def get_ip(self):
+    def get_ip(self) -> IPv4Address:
         return self.ip_v4
 
     @property
-    def has_ip(self):
+    def has_ip(self) -> bool:
         return self.ip_v4 is not None
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.__class__}, {self.__dict__}"
 
     # type hinting to access the _raw_log
-    def __lt__(self, other: "SSHLogEntry"):
+    def __lt__(self, other: "SSHLogEntry") -> bool:
         # if self.pid < other.pid:
         #     return True
         # if self.pid > other.pid:
@@ -109,8 +110,8 @@ class SSHLogEntry(metaclass=ABCMeta):
 
         return (self.pid, self.date, self._raw_log) < (other.pid, other.date, other._raw_log)
 
-    def __gt__(self, other: "SSHLogEntry"):
+    def __gt__(self, other: "SSHLogEntry") -> bool:
         return (self.pid, self.date, self._raw_log) > (other.pid, other.date, other._raw_log)
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         return not (self.__lt__(other) or self.__gt__(other))
--- L9/fun_8.py
+++ L9/fun_8.py
@@ -1,12 +1,18 @@
 from datetime import datetime
+from typing import List, TypeVar
 
+from SSHLogEntry import SSHLogEntry
 from SSHUser import SSHUser
 from SSHLogJournal import SSHLogJournal
 from SSHLogFactory import SSHLogFactory
 
 
-def demonstrate(ssh_log_entry_list, ssh_log_journal, ssh_users_list):
-    validation_list = ssh_log_entry_list + ssh_log_journal.entries + ssh_users_list
+def demonstrate(
+    log_entries: List[SSHLogEntry], log_journal: SSHLogJournal, log_users: List[SSHUser]
+) -> bool:
+    validation_list: List[SSHLogEntry | SSHLogJournal | SSHUser] = (
+        log_entries + log_journal.entries + log_users
+    )
 
     for elem in validation_list:
         if not elem.validate():
@@ -15,20 +21,28 @@ def demonstrate(ssh_log_entry_list, ssh_log_journal, ssh_users_list):
 
 
 if __name__ == "__main__":
-    message0 = """Dec 20 06:55:33 LabSZ sshd[24100]: 
+    message0: str = """Dec 20 06:55:33 LabSZ sshd[24100]: 
         Failed password for invalid user webmaster from 173.234.31.186 port 38926 ssh2"""
-    message1 = """Dec 10 06:55:48 LabSZ sshd[24100]: 
+    message1: str = """Dec 10 06:55:48 LabSZ sshd[24100]: 
         Failed password for invalid user webmaster from 173.234.31.186 port 38926 ssh2"""
-    message2 = """Dec 31 23:56:28 LabSZ sshd[3101]: Invalid user vyatta from 202.107.207.123"""
+    message2: str = (
+        """Dec 31 23:56:28 LabSZ sshd[3101]: Invalid user vyatta from 202.107.207.123"""
+    )
 
-    journal = SSHLogJournal()
+    journal: SSHLogJournal = SSHLogJournal()
     journal.append(message0)
     journal.append(message1)
     journal.append(message2)
 
-    logs = [SSHLogFactory.get_log_entry(message0), SSHLogFactory.get_log_entry(message1),
-            SSHLogFactory.get_log_entry(message2)]
+    logs: List[SSHLogEntry] = [
+        SSHLogFactory.get_log_entry(message0),
+        SSHLogFactory.get_log_entry(message1),
+        SSHLogFactory.get_log_entry(message2),
+    ]
 
-    users = [SSHUser("a", datetime.now()), SSHUser("_testimoney", datetime.now())]
+    users: List[SSHUser] = [
+        SSHUser("a", datetime.now()),
+        SSHUser("_testimoney", datetime.now()),
+    ]
 
     print(f"Did ducktyping work: {demonstrate(logs, journal, users)}")
