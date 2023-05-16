7510dd7e1682dcf806943fe10ae6e70adc6bd425
Pawel Dudek <pawel200216@interia.eu>
2023-05-16 19:36:40 +0200

    L9 added type hints to tests

--- L9/test_SSHLogEntry.py
+++ L9/test_SSHLogEntry.py
@@ -1,14 +1,15 @@
 from SSHLogFactory import SSHLogFactory
 from SSHLogEntry import SSHLogEntry
 from datetime import datetime
-from ipaddress import IPv4Address, ip_address
+from ipaddress import IPv4Address, ip_address, IPv6Address
 import pytest
 
 
-def test_get_date():
+def test_get_date() -> None:
     # given
     datetime_object: datetime = datetime.strptime(
-        "10/12/2023 07:02:47", '%d/%m/%Y %H:%M:%S')
+        "10/12/2023 07:02:47", "%d/%m/%Y %H:%M:%S"
+    )
     raw_log: str = "Dec 10 07:02:47 LabSZ sshd[24203]: Connection closed by 212.47.254.145 [preauth]"
 
     # when
@@ -17,9 +18,9 @@ def test_get_date():
     assert log.date == datetime_object
 
 
-def test_get_ipv4_correct():
+def test_get_ipv4_correct() -> None:
     # given
-    ip_v4: IPv4Address = ip_address("212.47.254.145")
+    ip_v4: IPv4Address | IPv6Address = ip_address("212.47.254.145")
     raw_log: str = "Dec 10 07:02:47 LabSZ sshd[24203]: Connection closed by 212.47.254.145 [preauth]"
     # when
     log: SSHLogEntry = SSHLogFactory.get_log_entry(raw_log)
@@ -27,9 +28,9 @@ def test_get_ipv4_correct():
     assert log.ip_v4 == ip_v4
 
 
-def test_get_ipv4_incorrect():
+def test_get_ipv4_incorrect() -> None:
     # given
-    ip_v4: IPv4Address = None
+    ip_v4: IPv4Address | None = None
     raw_log: str = "Dec 10 07:02:47 LabSZ sshd[24203]: Connection closed by 666.777.88.213 [preauth]"
     # when
     log: SSHLogEntry = SSHLogFactory.get_log_entry(raw_log)
@@ -44,9 +45,9 @@ def test_get_ipv4_incorrect():
     # but then we need to raise an exception in SSHLogEntry.get_ipv4s_from_log, not catch it
 
 
-def test_get_ipv4_empty():
+def test_get_ipv4_empty() -> None:
     # given
-    ip_v4: IPv4Address = None
+    ip_v4: IPv4Address | None = None
     raw_log: str = "Dec 10 07:02:47 LabSZ sshd[24203]: Connection closed [preauth]"
     # when
     log: SSHLogEntry = SSHLogFactory.get_log_entry(raw_log)
--- L9/test_SSHLogJournal.py
+++ L9/test_SSHLogJournal.py
@@ -3,17 +3,36 @@ from SSHLogError import SSHLogError
 from SSHLogOther import SSHLogOther
 from SSHLogPasswordAccepted import SSHLogPasswordAccepted
 from SSHLogPasswordDenied import SSHLogPasswordDenied
+from SSHLogEntry import SSHLogEntry
+from typing import Type
 import pytest
 
 
-@pytest.mark.parametrize("log, expectedType", [
-    ("Dec 10 09:32:20 LabSZ sshd[24680]: Accepted password for fztu from 119.137.62.142 port 49116 ssh2",
-     SSHLogPasswordAccepted),
-    ("Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from 173.234.31.186 port 38926 ssh2", SSHLogPasswordDenied),
-    ("Dec 10 07:51:15 LabSZ sshd[24324]: error: Received disconnect from 195.154.37.122: 3: com.jcraft.jsch.JSchException: Auth fail [preauth]", SSHLogError),
-    ("Dec 10 07:51:12 LabSZ sshd[24324]: pam_unix(sshd:auth): check pass; user unknown", SSHLogOther)
-])
-def test_append(log, expectedType):
+@pytest.mark.parametrize(
+    "log, expectedType",
+    [
+        (
+                "Dec 10 09:32:20 LabSZ sshd[24680]: Accepted password for fztu from 119.137.62.142 port 49116 ssh2",
+                SSHLogPasswordAccepted,
+        ),
+        (
+                "Dec 10 06:55:48 LabSZ sshd[24200]: "
+                "Failed password for invalid user webmaster from 173.234.31.186 port 38926 ssh2",
+                SSHLogPasswordDenied,
+        ),
+        (
+                "Dec 10 07:51:15 LabSZ sshd[24324]:"
+                "error: Received disconnect from 195.154.37.122: 3: com.jcraft.jsch.JSchException: Auth fail [preauth]",
+                SSHLogError,
+        ),
+        (
+                "Dec 10 07:51:12 LabSZ sshd[24324]: "
+                "pam_unix(sshd:auth): check pass; user unknown",
+                SSHLogOther,
+        ),
+    ],
+)
+def test_append(log: str, expectedType: Type[SSHLogEntry]) -> None:
     journal: SSHLogJournal = SSHLogJournal([])
     journal.append(log)
     assert type(journal[0]) is expectedType
